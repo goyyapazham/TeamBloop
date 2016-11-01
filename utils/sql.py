d3b43464c915a1a39548e6e57124868bb0f79d9c
71619997a <marksg07@gmail.com>
2016-11-01 15:35:53 -0400

    Add add_story, get_stories, get_title

--- utils/sql.py
+++ utils/sql.py
@@ -4,15 +4,34 @@ import csv
 
 def init(db):
     cur = db.cursor()
-    cur.execute('CREATE TABLE users (id INTEGER, username TEXT, password TEXT)')
-    cur.execute('CREATE TABLE stories (id INTEGER, title TEXT, latestid INTEGER)')
-    cur.execute('CREATE TABLE updates (id INTEGER, userid INTEGER, storyid INTEGER, content TEXT)')
+    cur.execute(
+        'CREATE TABLE users (id INTEGER, username TEXT, password TEXT)')
+    cur.execute(
+        'CREATE TABLE stories (id INTEGER, title TEXT, latestid INTEGER)')
+    cur.execute('''CREATE TABLE updates
+        (id INTEGER, userid INTEGER, storyid INTEGER, content TEXT)''')
     db.commit()
 
 
 def get_stories(db, userid, viewing_on):
     cur = db.cursor()
-    req = cur.execute('SELECT stories.id FROM stories, updates WHERE ')
+    q = '''SELECT updates.storyid FROM updates WHERE
+    updates.userid = ''' + str(userid)
+    res = cur.execute(q)
+    return [i[0] for i in res]
 
 
-def get_all_updates
+def add_story(db, title, userid, init_update):
+    cur = db.cursor()
+    res = cur.execute('SELECT id FROM stories')
+    newid = max([i[0] for i in res]) + 1
+    cur.execute('''INSERT INTO updates VALUES
+    (0, ''' + str(userid) + ', ' + str(newid) + ', ' + init_update + ')')
+    cur.execute(
+        'INSERT INTO stories VALUES (' + str(newid) + ', ' + title + ', 0)')
+    db.commit()
+
+
+def get_title(db, storyid):
+    return db.cursor().execute(
+        'SELECT title FROM stories WHERE id = ' + str(storyid))[0][0]
